67737fee38c7c1477b82f251dc6fd709f5105bb2
alsino <iamahorse@gmail.com>
2017-07-17 14:00:32 +0200

    Add playback controls

--- js/animation.js
+++ js/animation.js
@@ -1,11 +1,18 @@
 
-var blueBox = anime({
+var playPause = anime({
   targets: '#accent',
   backgroundColor: '#FFBE00',
   height: '100px',
   width: '100px',
   //borderRadius: 40,
   easing: 'easeInOutQuad',
-  loop: true,
-  duration: 5000
+  loop: false,
+  duration: 5000,
+  autoplay: false
 });
+
+
+var chartPlay = document.querySelector('#play');
+var chartPause = document.querySelector('#pause');
+chartPlay.onclick = playPause.play;
+chartPause.onclick = playPause.pause;

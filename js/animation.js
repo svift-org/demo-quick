c089100449f099366ae892ef26c4b8f960e6b9aa
alsino <iamahorse@gmail.com>
2017-07-17 15:08:01 +0200

    little treemap added

--- js/animation.js
+++ js/animation.js
@@ -1,18 +1,53 @@
+// Little Tree
+// var littleTree = anime({
+//   targets: '#back',
+//   borderRadius: 400,
+//   easing: 'easeInOutQuad',
+//   loop: false,
+//   duration: 3000,
+//   autoplay: true
+// });
 
-var playPause = anime({
+
+// Little Tree
+var littleTree = anime({
   targets: '#accent',
   backgroundColor: '#FFBE00',
-  height: '100px',
-  width: '100px',
-  //borderRadius: 40,
+  height: '50px',
+  width: '50px',
+  // borderRadius: 40,
   easing: 'easeInOutQuad',
   loop: false,
-  duration: 5000,
-  autoplay: false
+  duration: 3000,
+  autoplay: true
 });
 
 
-var chartPlay = document.querySelector('#play');
-var chartPause = document.querySelector('#pause');
-chartPlay.onclick = playPause.play;
-chartPause.onclick = playPause.pause;
+// var chartPlay = document.querySelector('#play');
+// var chartPause = document.querySelector('#pause');
+// chartPlay.onclick = littleTree.play;
+// chartPause.onclick = littleTree.pause;
+
+
+
+// // Little Tree Annotation
+var littleTreeAnnotationLine  = anime({
+  targets: '.legend',
+  width: '100px',
+  easing: 'easeInOutQuad',
+  delay: 1500,
+  loop: false,
+  duration: 1000,
+  autoplay: true
+});
+
+var littleTreeAnnotationText  = anime({
+  targets: '#legend-text',
+  left: "360px",
+  color: "#FFBE00",
+  easing: 'easeInOutQuad',
+  delay: 1500,
+  loop: false,
+  duration: 1000,
+  autoplay: true
+});

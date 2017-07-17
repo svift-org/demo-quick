d944396a5d0062cf6430d0ab96bd472e3197499b
alsino <iamahorse@gmail.com>
2017-07-17 13:25:49 +0200

    anime.js added

---
+++ js/animation.js
@@ -0,0 +1,11 @@
+
+var blueBox = anime({
+  targets: '#accent',
+  backgroundColor: '#FFBE00',
+  height: '100px',
+  width: '100px',
+  //borderRadius: 40,
+  easing: 'easeInOutQuad',
+  loop: true,
+  duration: 5000
+});

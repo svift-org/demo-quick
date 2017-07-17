e7d3ffc239fe7eabbde60a469f9c03413eb8b5bf
hhack <hans.f.hack@googlemail.com>
2017-07-17 20:17:05 +0200

    barcharts

---
+++ js/chart_3.js
@@ -0,0 +1,32 @@
+    var barAnimation = anime({
+      targets: '#bars_wrapper .bar',
+      delay: function(el, i) { 
+        return i * 400; //delay to start next bars_wrapper animation
+      },
+      opacity: 0,
+      backgroundColor: "#fff",
+      height: {
+        value:0,
+        // direction: 'reverse'
+      },
+      duration: 5200, //total animation time
+      easing: 'easeOutExpo',
+      loop: true,
+      direction: 'reverse' // Play the animation in reverse
+    });
+
+    var textBarAnimation = anime({
+      targets: '#text_wrapper span',
+      delay: function(el, i) { 
+        return i * 400; //delay to start next bars_wrapper animation
+      },
+      opacity: 0,
+      height: {
+        value:0,
+        // direction: 'reverse'
+      },
+      duration: 5200, //total animation time
+      easing: 'easeOutExpo',
+      loop: true,
+      direction: 'reverse' // Play the animation in reverse
+    });
